4f22b065d1c56a5cfa8f166215f46def2382824c
Saad123Khan <sk5908774@gmail.com>
2026-04-23 21:53:14 +0500

    route change

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ async function bootstrap() {
     },
   });
   
-  const port = parseInt(process.env.PORT || '3000', 10);
+  const port = parseInt(process.env.PORT || '4000', 10);
   const host = process.env.HOST || '0.0.0.0';
   
   await app.listen(port, host);
